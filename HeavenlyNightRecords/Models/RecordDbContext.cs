8beee32e35dc0027355b2dbf2883112b4a2b9274
ERedEarth99 <ezra.redearth@southeasttech.edu>
2024-10-18 23:46:14 -0500

    Add project files.

---
+++ HeavenlyNightRecords/Models/RecordDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HeavenlyNightRecords.Models
+{
+    public class RecordDbContext : DbContext
+    {
+        public RecordDbContext(DbContextOptions<RecordDbContext> options) {}
+
+    }
+}
---
+++ HeavenlyNightRecords/Models/Records.cs
@@ -0,0 +1,21 @@
+﻿namespace HeavenlyNightRecords.Models
+{
+    public class Records
+    {
+        public int Id { get; set; }
+        public string Artist { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+    }
+    public class Genres
+    {
+        public int Id { get; set; }
+        public string? Rock { get; set; }
+        public string? Metal { get; set; }
+        public string? Jazz { get; set; }
+        public string? Blues { get; set; }
+        public string? HipHop { get; set; }
+        public string? RnB { get; set; }
+        public string? Country { get; set; }
+    }
+}
